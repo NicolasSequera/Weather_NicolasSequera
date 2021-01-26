fcbefbe1cbdc102a6d8ec3b617f10082c4bf8b87
NicolasSequera <nicolas.sequera9@gmail.com>
2021-01-26 14:48:32 -0300

    26012021

---
+++ app/utils/Conditions.js
@@ -0,0 +1,61 @@
+export const Conditions = {
+  Rain: {
+    color: "#005BEA",
+    title: "Lluvioso",
+    subtitle:
+      "Traiga un paraguas y use una chaqueta para la lluvia y zapatos resistentes al agua.",
+    icon: "weather-rainy",
+    image: "http://res.cloudinary.com/inthegra/image/upload/v1611440459/Rain_rkptxc.jpg",
+  },
+  Clear: {
+    color: "#f7b733",
+    title: "Despejado",
+    subtitle:
+      "Disfrute del sol, pero no por mucho tiempo sin protector solar y gafas de sol.",
+    icon: "weather-sunny",
+    image: "http://res.cloudinary.com/inthegra/image/upload/v1611440724/Clear1_zgycoz.jpg",
+  },
+  Thunderstorm: {
+    color: "#616161",
+    title: "Tormentoso",
+    subtitle:
+      "Es mejor permanecer en el interior, especialmente durante los aguaceros, de lo contrario, traiga un paraguas.",
+    icon: "weather-lightning",
+    image: "http://res.cloudinary.com/inthegra/image/upload/v1611440459/Thunderstorm_udfd6k.jpg",
+  },
+  Clouds: {
+    color: "#1F1C2C",
+    title: "Nublado",
+    subtitle: "Mayormente nublado con sol ocasional.",
+    icon: "weather-cloudy",
+    image: "http://res.cloudinary.com/inthegra/image/upload/v1611440458/Clouds_cqorlf.jpg",
+  },
+  Snow: {
+    color: "#00d2ff",
+    title: "Nevado",
+    subtitle: "Disfruta de la nieve fresca pero ten cuidado al caminar.",
+    icon: "weather-snowy",
+    image: "http://res.cloudinary.com/inthegra/image/upload/v1611440459/Snow_csfasm.jpg",
+  },
+  Drizzle: {
+    color: "#076585",
+    title: "Llovizna",
+    subtitle: "Trae un paraguas.",
+    icon: "weather-hail",
+    image: "http://res.cloudinary.com/inthegra/image/upload/v1611440954/Drizzle_stbmp1.jpg",
+  },
+  Haze: {
+    color: "#66A6FF",
+    title: "Calina",
+    subtitle: "Use una chaqueta de lluvia.",
+    icon: "weather-hail",
+    image: "http://res.cloudinary.com/inthegra/image/upload/v1611440459/Haze_nln90z.jpg",
+  },
+  Mist: {
+    color: "#3CD3AD",
+    title: "Niebla",
+    subtitle: "Baja visibilidad, tenga cuidado al conducir.",
+    icon: "weather-fog",
+    image: "http://res.cloudinary.com/inthegra/image/upload/v1611440458/Mist_nbjesw.jpg",
+  },
+};
